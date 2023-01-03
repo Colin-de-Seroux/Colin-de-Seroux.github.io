3599b4373c2386eb893bfaa06839541d34e6d9d8
Colin de Seroux <colin.de-seroux@etu.univ-cotedazur.fr>
2023-01-03 10:58:10 +0100

    :white_check_mark: update for phone

--- CartoTD1/Exo2/script.js
+++ CartoTD1/Exo2/script.js
@@ -1,14 +1,22 @@
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   getPosition();
 });
 
 function successGCP(pos) {
   let crd = pos.coords;
 
-  document.getElementById("longitudeGCP").innerHTML = `Longitude : ${crd.longitude}`;
-  document.getElementById("latitudeGCP").innerHTML = `Latitude : ${crd.latitude}`;
-  document.getElementById("altitudeGCP").innerHTML = `Altitude : ${crd.altitude}`;
-  document.getElementById("precisionGCP").innerHTML = `Précision : ${crd.accuracy}`;
+  document.getElementById(
+    "longitudeGCP"
+  ).innerHTML = `Longitude : ${crd.longitude}`;
+  document.getElementById(
+    "latitudeGCP"
+  ).innerHTML = `Latitude : ${crd.latitude}`;
+  document.getElementById(
+    "altitudeGCP"
+  ).innerHTML = `Altitude : ${crd.altitude}`;
+  document.getElementById(
+    "precisionGCP"
+  ).innerHTML = `Précision : ${crd.accuracy}`;
   document.getElementById("vitesseGCP").innerHTML = `Vitesse : ${crd.speed}`;
   document.getElementById("dateGCP").innerHTML = new Date();
 }
@@ -20,10 +28,18 @@ function errorGCP(err) {
 function successWP(pos) {
   let crd = pos.coords;
 
-  document.getElementById("longitudeWP").innerHTML = `Longitude : ${crd.longitude}`;
-  document.getElementById("latitudeWP").innerHTML = `Latitude : ${crd.latitude}`;
-  document.getElementById("altitudeWP").innerHTML = `Altitude : ${crd.altitude}`;
-  document.getElementById("precisionWP").innerHTML = `Précision : ${crd.accuracy}`;
+  document.getElementById(
+    "longitudeWP"
+  ).innerHTML = `Longitude : ${crd.longitude}`;
+  document.getElementById(
+    "latitudeWP"
+  ).innerHTML = `Latitude : ${crd.latitude}`;
+  document.getElementById(
+    "altitudeWP"
+  ).innerHTML = `Altitude : ${crd.altitude}`;
+  document.getElementById(
+    "precisionWP"
+  ).innerHTML = `Précision : ${crd.accuracy}`;
   document.getElementById("vitesseWP").innerHTML = `Vitesse : ${crd.speed}`;
   document.getElementById("dateWP").innerHTML = new Date();
 }
@@ -39,6 +55,6 @@ function getPosition() {
     maximumAge: 0,
   };
 
-  navigator.geolocation.getCurrentPosition(successGCP, errorGCP, options); 
+  navigator.geolocation.getCurrentPosition(successGCP, errorGCP, options);
   navigator.geolocation.watchPosition(successWP, errorWP, options);
 }
