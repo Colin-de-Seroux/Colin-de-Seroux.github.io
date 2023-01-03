b2e9168a72eb93fabf3b4a7d6531fe9957338abe
Colin de Seroux <colin.de-seroux@etu.univ-cotedazur.fr>
2023-01-03 11:09:19 +0100

    :bug: test

--- CartoTD1/Exo3/script.js
+++ CartoTD1/Exo3/script.js
@@ -3,9 +3,9 @@ window.addEventListener("load", function () {
 });
 
 function process(event) {
-  document.getElementById("alpha").innerHTML = event.accelerationIncludingGravity.x;
-  document.getElementById("beta").innerHTML = event.accelerationIncludingGravity.y;
-  document.getElementById("gamma").innerHTML = event.accelerationIncludingGravity.z;
+  document.getElementById("alpha").innerHTML = event.alpha;
+  document.getElementById("beta").innerHTML = event.beta;
+  document.getElementById("gamma").innerHTML = event.gamma;
 }
 
 function getOrientation() {
