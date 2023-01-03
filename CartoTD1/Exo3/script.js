054b22b0f659101e8621e8c8a852e6ab2f1875d7
Colin de Seroux <colin.de-seroux@etu.univ-cotedazur.fr>
2023-01-03 11:37:14 +0100

    :bug: test

--- CartoTD1/Exo3/script.js
+++ CartoTD1/Exo3/script.js
@@ -9,8 +9,13 @@ function processO(event) {
 }
 
 function processM(event) {
-  document.getElementById("acceleration").innerHTML = event.accelerationIncludingGravity;
-  document.getElementById("rotation").innerHTML = event.rotationRate;
+  document.getElementById("ax").innerHTML = `Accélération en x : ${event.accelerationIncludingGravity.x}`;
+  document.getElementById("ay").innerHTML = `Accélération en y : ${event.accelerationIncludingGravity.y}`;
+  document.getElementById("az").innerHTML = `Accélération en z : ${event.accelerationIncludingGravity.z}`;
+
+  document.getElementById("ax").innerHTML = `Rotation en x : ${event.rotationRate.alpha}`;
+  document.getElementById("ay").innerHTML = `Rotation en y : ${event.rotationRate.beta}`;
+  document.getElementById("az").innerHTML = `Rotation en z : ${event.rotationRate.gamma}`;
 }
 
 function getOrientation() {
