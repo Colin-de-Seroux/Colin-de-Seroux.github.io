d085a62da33410575267b32382bd82c19951904f
Colin de Seroux <colin.de-seroux@etu.univ-cotedazur.fr>
2023-01-11 12:48:12 +0100

    add Exo++

--- CartoTD2/Exo3PlusPLus/script.js
+++ CartoTD2/Exo3PlusPLus/script.js
@@ -5,66 +5,97 @@ window.addEventListener("load", function () {
 async function success(pos) {
   const crd = pos.coords;
 
-  const greenIcon = new L.Icon({
-    iconUrl:
-      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-    shadowUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiY3YwNiIsImEiOiJjajg2MmpzYjcwbWdnMzNsc2NzM2l4eW0yIn0.TfDJipR5II7orUZaC848YA";
+
+  let map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v9",
+    center: [crd.longitude, crd.latitude],
+    zoom: 13,
   });
 
-  const redIcon = new L.Icon({
-    iconUrl:
-      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-    shadowUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
+  const popupIci = new mapboxgl.Popup({ offset: 25 }).setText("Je suis ici");
+  const popupNice = new mapboxgl.Popup({ offset: 25 }).setText("Nice");
+
+  new mapboxgl.Marker()
+    .setLngLat([crd.longitude, crd.latitude])
+    .addTo(map)
+    .setPopup(popupIci);
+
+  new mapboxgl.Marker()
+    .setLngLat([7.268157, 43.701711])
+    .addTo(map)
+    .setPopup(popupNice);
+
+  map.on("load", () => {
+    getRoute(map, [crd.longitude, crd.latitude], [7.268157, 43.701711]);
+
+    map.addLayer({
+      id: "point",
+      type: "circle",
+      source: {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "Point",
+                coordinates: start,
+              },
+            },
+          ],
+        },
+      },
+      paint: {
+        "circle-radius": 10,
+        "circle-color": "#3887be",
+      },
+    });
   });
+}
 
-  // L.tileLayer("https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png", {
-  //   attribution:
-  //     "&copy; 2023 Copyright : Colin de Seroux, LP DAM, phenix333.dev@gmail.com",
-  // }).addTo(map);
-
-  // L.marker([crd.latitude, crd.longitude], { icon: redIcon })
-  //   .addTo(map)
-  //   .bindPopup("Je suis ici");
-
-  // L.circle([crd.latitude, crd.longitude], {
-  //   color: "orange",
-  //   radius: crd.accuracy,
-  // }).addTo(map);
-
-  // const nice = [43.701711, 7.268157];
-
-  // L.marker(nice, { icon: greenIcon }).addTo(map).bindPopup("Nice");
-
-  L.mapquest.key = "tR2C6osuQcc3RoWnxDMXF6FACtNAzMl8";
-
-  // 'map' refers to a <div> element with the ID map
-  let map = L.mapquest.map("map", {
-    center: [crd.latitude, crd.longitude],
-    layers: L.mapquest.tileLayer("map"),
-    zoom: 12,
-  });
-
-  L.marker([crd.latitude, crd.longitude]).addTo(map);
-  L.marker([43.701711, 7.268157]).addTo(map);
-
-  map.addLayer(map.Routing.RouteLayer.extend({
-    createStartMarker: () => {
-      return L.marker([crd.latitude, crd.longitude]).addTo(map);
+async function getRoute(map, start, end) {
+  const query = await fetch(
+    `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=pk.eyJ1IjoiY3YwNiIsImEiOiJjajg2MmpzYjcwbWdnMzNsc2NzM2l4eW0yIn0.TfDJipR5II7orUZaC848YA`,
+    { method: "GET" }
+  );
+  const json = await query.json();
+  const data = json.routes[0];
+  const route = data.geometry.coordinates;
+  const geojson = {
+    type: "Feature",
+    properties: {},
+    geometry: {
+      type: "LineString",
+      coordinates: route,
     },
-    createEndMarker: () => {
-      return L.marker([43.701711, 7.268157]).addTo(map);
-    },
-  }));
+  };
+
+  if (map.getSource("route")) {
+    map.getSource("route").setData(geojson);
+  } else {
+    map.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: geojson,
+      },
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#3887be",
+        "line-width": 5,
+        "line-opacity": 0.75,
+      },
+    });
+  }
 }
 
 function error(err) {
