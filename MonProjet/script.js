95c87fcb21269d54a2e547159ffa0d237f7b663f
Colin de Seroux <colindeseroux@gmail.com>
2023-02-06 23:15:25 +0100

    test

--- MonProjet/script.js
+++ MonProjet/script.js
@@ -3,6 +3,8 @@ let map;
 let listSite = [];
 let goTo;
 let myPos;
+let modelTransform;
+let car;
 
 window.addEventListener("load", async function () {
   mapboxgl.accessToken =
@@ -38,7 +40,45 @@ function getListClimbing() {
   ];
 }
 
-async function success(pos) {
+function createWheels() {
+  const geometry = new THREE.BoxBufferGeometry(12, 12, 33);
+  const material = new THREE.MeshLambertMaterial({ color: 0x333333 });
+  const wheel = new THREE.Mesh(geometry, material);
+  return wheel;
+}
+
+function createCar() {
+  car = new THREE.Group();
+
+  const backWheel = createWheels();
+  backWheel.position.y = 6;
+  backWheel.position.x = -18;
+  car.add(backWheel);
+
+  const frontWheel = createWheels();
+  frontWheel.position.y = 6;
+  frontWheel.position.x = 18;
+  car.add(frontWheel);
+
+  const main = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(60, 15, 30),
+    new THREE.MeshLambertMaterial({ color: 0x78b14b })
+  );
+  main.position.y = 12;
+  car.add(main);
+
+  const cabin = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(33, 12, 24),
+    new THREE.MeshLambertMaterial({ color: 0xffffff })
+  );
+  cabin.position.x = -6;
+  cabin.position.y = 25.5;
+  car.add(cabin);
+
+  return car;
+}
+
+function success(pos) {
   const crd = pos.coords;
 
   if (state === "init") {
@@ -51,10 +91,16 @@ async function success(pos) {
       zoom: 13,
     });
 
+    ///////////////////////
+    ///////////////////////
+    ///////////////////////
     myPos = new mapboxgl.Marker({ color: "#00ff00" })
       .setLngLat([crd.longitude, crd.latitude])
       .addTo(map)
       .setPopup(new mapboxgl.Popup({ offset: 25 }).setText("Je suis ici"));
+    ///////////////////////
+    ///////////////////////
+    ///////////////////////
 
     getListClimbing().forEach((site) => {
       let popupSite = new mapboxgl.Popup({ offset: 25 }).setText(site.name);
@@ -84,16 +130,108 @@ async function success(pos) {
       listSite.push(markerSite);
     });
 
+    const modelOrigin = [crd.longitude, crd.latitude];
+    const modelAltitude = 0;
+    const modelRotate = [Math.PI / 2, 0, 0];
+
+    const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+      modelOrigin,
+      modelAltitude
+    );
+
+    modelTransform = {
+      translateX: modelAsMercatorCoordinate.x,
+      translateY: modelAsMercatorCoordinate.y,
+      translateZ: modelAsMercatorCoordinate.z,
+      rotateX: modelRotate[0],
+      rotateY: modelRotate[1],
+      rotateZ: modelRotate[2],
+      scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits(),
+    };
+
+    const THREE = window.THREE;
+
+    const customLayer = {
+      id: "3d-model",
+      type: "custom",
+      renderingMode: "3d",
+      onAdd: function (map, gl) {
+        this.camera = new THREE.Camera();
+        this.scene = new THREE.Scene();
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
+
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
+
+        // const loader = new THREE.GLTFLoader();
+        // loader.load(
+        //   "https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf",
+        //   (gltf) => {
+        //     this.scene.add(gltf.scene);
+        //   }
+        // );
+        const car = createCar();
+        this.scene.add(car);
+
+        this.map = map;
+
+        this.renderer = new THREE.WebGLRenderer({
+          canvas: map.getCanvas(),
+          context: gl,
+          antialias: true,
+        });
+
+        this.renderer.autoClear = false;
+      },
+      render: function (gl, matrix) {
+        const rotationX = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(1, 0, 0),
+          modelTransform.rotateX
+        );
+        const rotationY = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(0, 1, 0),
+          modelTransform.rotateY
+        );
+        const rotationZ = new THREE.Matrix4().makeRotationAxis(
+          new THREE.Vector3(0, 0, 1),
+          modelTransform.rotateZ
+        );
+
+        const m = new THREE.Matrix4().fromArray(matrix);
+        const l = new THREE.Matrix4()
+          .makeTranslation(
+            modelTransform.translateX,
+            modelTransform.translateY,
+            modelTransform.translateZ
+          )
+          .scale(
+            new THREE.Vector3(
+              modelTransform.scale,
+              -modelTransform.scale,
+              modelTransform.scale
+            )
+          )
+          .multiply(rotationX)
+          .multiply(rotationY)
+          .multiply(rotationZ);
+
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+      },
+    };
+
     map.on("style.load", () => {
-      // Insert the layer beneath any symbol layer.
       const layers = map.getStyle().layers;
       const labelLayerId = layers.find(
         (layer) => layer.type === "symbol" && layer.layout["text-field"]
       ).id;
 
-      // The 'building' layer in the Mapbox Streets
-      // vector tileset contains building height data
-      // from OpenStreetMap.
       map.addLayer(
         {
           id: "add-3d-buildings",
@@ -104,10 +242,6 @@ async function success(pos) {
           minzoom: 15,
           paint: {
             "fill-extrusion-color": "#aaa",
-
-            // Use an 'interpolate' expression to
-            // add a smooth transition effect to
-            // the buildings as the user zooms in.
             "fill-extrusion-height": [
               "interpolate",
               ["linear"],
@@ -131,6 +265,17 @@ async function success(pos) {
         },
         labelLayerId
       );
+
+      map.addLayer(customLayer, "waterway-label");
+    });
+
+    window.addEventListener("deviceorientation", function (event) {
+      car.rotation.x = event.beta * (Math.PI / 180);
+      car.rotation.y = -event.gamma * (Math.PI / 180);
+      car.rotation.z = event.alpha * (Math.PI / 180);
+      car.rotation.reorder("XYZ");
+      car.matrix.makeRotationFromEuler(car.rotation);
+      car.matrixAutoUpdate = false;
     });
   } else if (state === "goToSite") {
     getRoute(
@@ -188,9 +333,24 @@ function error(err) {
 function successWP(pos) {
   const crd = pos.coords;
 
+  ///////////////////////
+  ///////////////////////
+  ///////////////////////
   myPos.setLngLat([crd.longitude, crd.latitude]);
+  ///////////////////////
+  ///////////////////////
+  ///////////////////////
 
-  document.getElementById("speed").innerHTML = `Vitesse : ${crd.speed === null ? 0 : crd.speed} km/h`;
+  const position = new mapboxgl.MercatorCoordinate(crd.longitude, crd.latitude);
+
+  modelTransform.x = position.x;
+  modelTransform.y = position.y;
+
+  map.triggerRepaint();
+
+  document.getElementById("speed").innerHTML = `Vitesse : ${
+    crd.speed === null ? 0 : crd.speed
+  } km/h`;
 }
 
 function errorWP(err) {
